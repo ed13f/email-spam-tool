09490595d714726d3ede4a31f3f8ea04060109a1
Enrico De Santis <enricodesantis@Enricos-MacBook-Pro.local>
2020-07-19 22:55:19 -0400

    fix logo src

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component{
 					return(
 						<header className="header app__row">
 							<div className="app__column">
-								<a href="https://ricodesantis.com"><img className="header__logo" alt="RD Development Logo" src="/images/RD-logo-grey-650-dark.png" /></a>
+								<a href="https://ricodesantis.com"><img className="header__logo" alt="RD Development Logo" src="/public/images/RD-logo-grey-650-dark.png" /></a>
 							</div>
 							<div className="app__column">
 								<h1 className="header__title">Email Marketing Spam Tool</h1>
