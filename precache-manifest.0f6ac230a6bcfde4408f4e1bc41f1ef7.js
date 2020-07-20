f359ec777f2663d8369dd53d619942a07b65c659
Enrico De Santis <enricodesantis@Enricos-MacBook-Pro.local>
2020-07-19 22:36:59 -0400

    Updates

---
+++ precache-manifest.0f6ac230a6bcfde4408f4e1bc41f1ef7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7eda7aeb62f7e18c3ba7e14ed283db6",
+    "url": "/email-spam-tool/index.html"
+  },
+  {
+    "revision": "5f311fdf96a96606e2c0",
+    "url": "/email-spam-tool/static/css/2.e418317b.chunk.css"
+  },
+  {
+    "revision": "a86e4497b8f69921ca03",
+    "url": "/email-spam-tool/static/css/main.7e8cb955.chunk.css"
+  },
+  {
+    "revision": "5f311fdf96a96606e2c0",
+    "url": "/email-spam-tool/static/js/2.950f6e1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/email-spam-tool/static/js/2.950f6e1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86e4497b8f69921ca03",
+    "url": "/email-spam-tool/static/js/main.953f23c6.chunk.js"
+  },
+  {
+    "revision": "6ff10a28cfbe1b1e9df5",
+    "url": "/email-spam-tool/static/js/runtime-main.b9fd5618.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/email-spam-tool/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/email-spam-tool/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/email-spam-tool/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/email-spam-tool/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/email-spam-tool/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
